377f893c9d0e6bf7dea76c8f1d7d2d36d87d6727
MattHough1999 <43546852+MattHough1999@users.noreply.github.com>
2021-01-05 22:33:50 +0000

    added overworld scene
    Added a slider that determins the position of the player

---
+++ COMP3000 QuillStreak/Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform PTransform;
+    public float Value;
+    void Start()
+    {
+        PTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void move(float value) 
+    {
+        transform.position = new Vector3(value, 0,0);
+    }
+}
